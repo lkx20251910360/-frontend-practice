c5a22ae2a02937d27460b87c801c15f32a58970b
unknown <724713340@qq.com>
2026-09-21 20:14:36 +0800

    添加localStorage持久化

--- todo-app/app.js
+++ todo-app/app.js
@@ -65,4 +65,7 @@ filters.addEventListener('click', (e) => {
     currentFilter = e.target.dataset.filter; 
     render();
 });
+let tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+const save = () => localStorage.setItem('tasks', JSON.stringify(tasks));
+
 
